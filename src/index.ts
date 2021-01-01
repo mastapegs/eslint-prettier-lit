269b81dddcd931d1e62b1a92db07969e0703febf
Matthew Pagan <mastapegs01@gmail.com>
2021-01-01 09:48:24 -0500

    create a helper function

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,22 @@ import { LitElement, html, customElement } from "lit-element";
 
 @customElement("app-component")
 export default class App extends LitElement {
-  static render() {
+  _messageHTMLTemplate() {
+    return html`
+      <p>Integrates with:</p>
+      <ul>
+        <li>TypeScript</li>
+        <li>ESLint</li>
+        <li>Prettier</li>
+      </ul>
+    `;
+  }
+
+  render() {
     return html`
       <h1>ESLint Testing</h1>
       <p>This project will test eslinting</p>
+      ${this._messageHTMLTemplate()}
     `;
   }
 }
